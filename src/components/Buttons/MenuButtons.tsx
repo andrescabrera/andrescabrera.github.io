21a707f21f015b3026556cf1a8d17ac686776180
Andrés Cabrera <andrescabrera@users.noreply.github.com>
2024-03-19 10:39:14 -0300

    Update MenuButtons.tsx

--- src/components/Buttons/MenuButtons.tsx
+++ src/components/Buttons/MenuButtons.tsx
@@ -6,7 +6,7 @@ const MenuButtons = () => {
         <section className="pt-12">
             <MenuButton url="/andrescabrera-eng.pdf" menu_type="CV - English" />
             <MenuButton url="/andrescabrera-spa.pdf" menu_type="CV - Spanish" />
-            <MenuButton url="/personal.pdf" menu_type="Family, Travel and Foods" />
+            <MenuButton url="https://roadmap.sh/backend?s=65f99212adfe421617302b21" menu_type="Tech Stack" />
         </section>
 
     )
